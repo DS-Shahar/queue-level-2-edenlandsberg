767211049d308f86df9442c18f174e6b7595c671
edenlandsberg <eden.landsberg@gmail.com>
2026-01-05 15:54:20 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,67 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+ ///////////////////////////////
+ 
+  public static int max_q( Queue <Integer> q){ 
+ Queue <Integer> h=ex1(q);
+ int max_num=0;
+ int current=0;
+ while(!h.isEmpty()){
+    current=h.remove();
+        if (current> max_num)
+        max_num=current;
+    }
+    return max_num;}
+
+
+
+ /////////////////////////////
+ 
+ public static int log( int x){ 
+
+ 
+ int num = (int)Math.log10(x) + 1;
+ 
+ return num;} 
+
+ ///////////////////////////
+ 
+
+ public static int location( int num, int x){ 
+
+ for (int i=0;i<x;i++){
+     num=num/10;
+ }
+ 
+ 
+ return num%10;} 
+
+ ///////////////////////////
+ 
+  public static Queue <Integer> sort(Queue<Integer> q) {
+      
+      Queue <Integer> h=ex1(q);
+      Queue<Integer>[] a= new Queue [10];
+      for (int i=0;i<10;i++){
+          a[i]=new Queue<Integer>();
+      }
+      
+      int max=max_q(q);
+      int x=log(max_q(q));
+      
+       for (int i=0;i<x;i++){
+      
+         while (!h.isEmpty()) {
+          int current=h.remove();
+          a[location(current,i)].insert(current);
+         }
+         
+         for (int j=0;j<10;j++){
+             while (!a[j].isEmpty()){
+                 h.insert(a[j].remove());
+             }
+         }
+ 
+    }
+    
+  return h;}  
+
+/////////////////////////////////
